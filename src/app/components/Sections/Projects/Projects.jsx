c85700e5b7f61b44e82b3907975bfff762be40af
Sanjay Saravanan <sanjay.saravanan@encora.com>
2023-11-15 17:04:29 +0530

    added a project

--- src/app/components/Sections/Projects/Projects.jsx
+++ src/app/components/Sections/Projects/Projects.jsx
@@ -1,30 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import Cards from '../../../components/Cards/Cards';
+import Cards from "../../../components/Cards/Cards";
 
 const Projects = () => {
   return (
     <Cards
       data={[
         {
-          url: 'https://sanjaysaravanan38.medium.com/containerize-an-application-using-dockerfile-bf35c38d4d95',
-          imageUrl: 'https://ik.imagekit.io/twsok8jou/docker.jpeg',
-          title: 'Containerize an application using Docker',
-          type: 'project',
-          description: 'Containerize any software application using Docker and make it easily portable across platforms.',
-          techs: ['React', 'Python']
-        },
-        {
-          url: 'https://sanjaysaravanan38.medium.com/ci-cd-for-a-web-application-using-aws-codepipeline-46c18985d74c',
-          imageUrl: 'https://ik.imagekit.io/twsok8jou/cicd.jpeg',
-          title: 'Application CI/CD using AWS Code Pipeline',
-          type: 'blog',
-          date: 'May 2, 2021',
-          description: 'Containerize any software application using Docker and make it easily portable across platforms.',
+          url: "https://phenomenal-cactus-c80763.netlify.app/",
+          imageUrl: "https://ik.imagekit.io/twsok8jou/Angular-Todo-List.png",
+          title: "ToDo Application",
+          type: "project",
+          description: "Simple ToDo applicaiton using Angular.",
+          techs: ["HTML", "CSS", "JS", "Angular"],
+          sourceUrl: 'https://github.com/sanjaysaravanan/todo-app-angular'
         }
       ]}
     />
-  )
+  );
 };
 
 export default Projects;
