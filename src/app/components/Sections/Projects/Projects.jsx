5a50ecf96cfb53cb58f6fa052998afc69c128103
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2024-07-23 20:29:27 +0530

    added one more project

--- src/app/components/Sections/Projects/Projects.jsx
+++ src/app/components/Sections/Projects/Projects.jsx
@@ -14,6 +14,15 @@ const Projects = () => {
           description: "Simple ToDo applicaiton using Angular.",
           techs: ["HTML", "CSS", "JS", "Angular"],
           sourceUrl: 'https://github.com/sanjaysaravanan/todo-app-angular'
+        },
+        {
+          url: "https://flourishing-meerkat-370485.netlify.app/",
+          imageUrl: "https://ik.imagekit.io/twsok8jou/dashboard.png",
+          title: "Graph Generation Tool",
+          type: "project",
+          description: "Create Charts & Graphs using Excel, CSV & JSON",
+          techs: ["HTML", "CSS", "JS", "React", "Python", "Fast-API", "Re-Charts"],
+          sourceUrl: 'https://github.com/sanjaysaravanan/dashboard-ui-js'
         }
       ]}
     />
